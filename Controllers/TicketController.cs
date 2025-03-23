34512bb2e7a34fe39ff577d4ba47b64f0f208835
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-03-23 16:49:36 +0300

    feat: Implement Ticket management view and controller for displaying tickets.

---
+++ Controllers/TicketController.cs
@@ -0,0 +1,21 @@
+﻿using Crm_CSharp.Models;
+using Crm_CSharp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crm_CSharp.Controllers;
+
+public class TicketController : Controller
+{
+    private readonly TicketService _ticketService;
+    
+    public TicketController(TicketService ticketService)
+    {
+        _ticketService = ticketService;
+    }
+    
+    public async Task<IActionResult> Index()
+    {
+        var tickets = await _ticketService.GetAllTicketsAsync();
+        return View(tickets);
+    }
+}
