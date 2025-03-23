c2085eb2c1fd7980b6cbac4638e4b15ca9b48ce2
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-03-23 17:02:43 +0300

    feat: Enhance expense editing with source parameter and conditional redirection.

--- Controllers/ExpenseController.cs
+++ Controllers/ExpenseController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Crm_CSharp.Models;
 using Crm_CSharp.Services;
+using HostingEnvironmentExtensions = Microsoft.AspNetCore.Hosting.HostingEnvironmentExtensions;
 
 namespace Crm_CSharp.Controllers
 {
@@ -24,38 +25,39 @@ namespace Crm_CSharp.Controllers
             return View("Form", new Expense());
         }
 
-        public async Task<IActionResult> Edit(long id)
+        public async Task<IActionResult> Edit(long id, string source)
         {
             var expense = await _expenseService.GetExpenseByIdAsync(id);
             if (expense == null)
             {
                 return NotFound();
             }
-            return View("Form", expense);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Save(Expense expense)
-        {
-
-            Expense result;
-            if (expense.ExpenseId == 0)
-            {
-                result = await _expenseService.CreateExpenseAsync(expense);
-            }
-            else
-            {
-                result = await _expenseService.UpdateExpenseAsync(expense.ExpenseId, expense);
-            }
             
-            if (result != null)
-            {
-                TempData["Message"] = "Dépense enregistrée avec succès";
-                return RedirectToAction("Index","Lead");
-            }
-
-            ModelState.AddModelError("Message", "Erreur lors de l'enregistrement de la dépense");
+            ViewData["Source"] = source;
             return View("Form", expense);
         }
+        
+        public async Task<IActionResult> Update(Expense expense, string source)
+        {
+            var updatedExpense = await _expenseService.UpdateExpenseAsync(expense.ExpenseId, expense);
+            
+            if (updatedExpense != null)
+            {
+                TempData["Message"] = "Montant mise à jour avec succès";
+                
+                // Redirection en fonction de la source
+                if (source == "ticket")
+                {
+                    return RedirectToAction("Index", "Ticket");
+                }
+                else if (source == "lead")
+                {
+                    return RedirectToAction("Index", "Lead");
+                }
+            }
+            ModelState.AddModelError("", "Erreur lors de la mise à jour de la dépense");
+            return View("Form", expense);
+        }
+        
     }
 }
