54ea066d74f12bd112c83d2f08e8a84abda1e4c6
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-03-23 21:45:47 +0300

    feat: Add BudgetController to manage budget retrieval and display.

---
+++ Controllers/BudgetController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Crm_CSharp.Services;
+using Crm_CSharp.Models;
+
+namespace Crm_CSharp.Controllers;
+
+public class BudgetController : Controller
+{
+    private readonly BudgetService _budgetService;
+
+    public BudgetController(BudgetService budgetService)
+    {
+        _budgetService = budgetService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var budgets = await _budgetService.GetBudgetsAsync();
+        return View(budgets);
+    }
+}
