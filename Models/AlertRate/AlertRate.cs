6db057eb3fc96986289b4b80aa4430abb15114ca
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-03-23 09:08:56 +0300

    feat: Add AlertRate model and AlertRateService for managing alert rates.

---
+++ Models/AlertRate/AlertRate.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRM_CSharp.Models.AlertRate;
+
+public class AlertRate
+{
+    public int AlertRateId { get; set; }
+
+    [Required(ErrorMessage = "Le taux d'alerte est requis")]
+    [Range(0, 1, ErrorMessage = "Le taux doit être compris entre 0 et 1")]
+    [Display(Name = "Taux d'alerte")]
+    public double Rate { get; set; }
+}
