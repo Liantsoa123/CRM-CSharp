35119d6e114ea156597518ad9e1124bf13f3458e
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-03-23 09:11:01 +0300

    feat: Add all necessary models.

---
+++ Models/Expense.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Crm_CSharp.Models;
+
+public partial class Expense
+{
+    public long ExpenseId { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public DateOnly Date { get; set; }
+
+    public string? Description { get; set; }
+
+    public long BudgetId { get; set; }
+
+    public virtual Budget Budget { get; set; } = null!;
+
+    public virtual ICollection<TriggerLead> TriggerLeads { get; set; } = new List<TriggerLead>();
+
+    public virtual ICollection<TriggerTicket> TriggerTickets { get; set; } = new List<TriggerTicket>();
+}
